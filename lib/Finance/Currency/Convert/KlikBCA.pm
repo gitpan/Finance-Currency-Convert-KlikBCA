1c003bc3dc08945d8c18beb5917107ed2c34dad9
Steven Haryanto <stevenharyanto@gmail.com>
2014-02-18 12:44:52 +0000

    Import of SHARYANTO/Finance-Currency-Convert-KlikBCA-0.03 from CPAN.
    gitpan-cpan-distribution: Finance-Currency-Convert-KlikBCA
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Finance-Currency-Convert-KlikBCA-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Finance/Currency/Convert/KlikBCA.pm
+++ lib/Finance/Currency/Convert/KlikBCA.pm
@@ -1,16 +1,16 @@
 package Finance::Currency::Convert::KlikBCA;
 
-use 5.010;
+use 5.010001;
 use strict;
 use warnings;
 use Log::Any '$log';
 use LWP::Simple;
 use Parse::Number::ID qw(parse_number_id);
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(convert_currency);
+our @EXPORT_OK = qw(get_currencies convert_currency);
 
 our %SPEC;
 
@@ -19,41 +19,77 @@ $SPEC{get_currencies} = {
     v => 1.1,
 };
 sub get_currencies {
-    my $page = get "http://www.bca.co.id/id/biaya-limit/kurs_counter_bca/kurs_counter_bca_landing.jsp"
-        or return [500, "Can't retrieve KlikBCA page"];
+    my %args = @_;
 
-    $page =~ s!(<table .+? DD/TT .+?</table>)!!xs
-        or return [500, "Can't scrape DD/TT table"];
-    my $ddtt_table = $1;
+    my $page;
+    if ($args{_page_content}) {
+        $page = $args{_page_content};
+    } else {
+        $page = get "http://www.bca.co.id/id/biaya-limit/kurs_counter_bca/kurs_counter_bca_landing.jsp"
+            or return [500, "Can't retrieve KlikBCA page"];
+    }
+
+    $page =~ s!(<table .+? Mata\sUang .+?</table>)!!xs
+        or return [543, "Can't scrape Mata Uang table"];
+    my $mu_table = $1;
+    $page =~ s!(<table .+? e-Rate .+?</table>)!!xs
+        or return [543, "Can't scrape e-Rate table"];
+    my $er_table = $1;
+    $page =~ s!(<table .+? TT \s Counter .+?</table>)!!xs
+        or return [543, "Can't scrape TT Counter table"];
+    my $ttc_table = $1;
     $page =~ s!(<table .+? Bank \s Notes .+?</table>)!!xs
-        or return [500, "Can't scrape Bank Notes table"];
+        or return [543, "Can't scrape e-Rate table"];
     my $bn_table = $1;
 
-    # XXX parse DD/TT update date
-    # XXX parse Bank Notes update date
-
     my @items;
-    while ($ddtt_table =~ m!<td[^>]+>([A-Z]{3})</td>\s+
-                            <td[^>]+>([0-9.,]+)</td>\s+
-                            <td[^>]+>([0-9.,]+)</td>
-                           !xsg) {
-        push @items, {
-            currency  => $1,
-            sell_ddtt => parse_number_id(text=>$2),
-            buy_ddtt  => parse_number_id(text=>$3),
-        };
+    while ($mu_table =~ m!<td[^>]+>([A-Z]{3})</td>!gsx) {
+        push @items, { currency => $1 };
     }
+    @items or return [543, "Check: no currencies found in Mata Uang Table"];
     my $num_items = @items;
-    my $i = 0;
-    while ($bn_table   =~ m!<td[^>]+>([0-9.,]+)</td>\s+
-                            <td[^>]+>([0-9.,]+)</td>\s+</tr>
-                           !xsg) {
-        $items[$i]{sell_bn} = parse_number_id(text=>$1);
-        $items[$i]{buy_bn}  = parse_number_id(text=>$2);
+    my $i;
+
+    $num_items >= 3 or return [543, "Sanity: too few items found in Mata Uang Table"];
+
+    $i = 0;
+    while ($er_table   =~ m{
+                               <td[^>]+>([0-9.,]+)</td>\s+
+                               <td[^>]+>([0-9.,]+)</td>\s*
+                               (?:<!--.+?-->)?\s*</tr>
+                       }xsg) {
+        $items[$i]{sell_er}  = parse_number_id(text=>$1);
+        $items[$i]{buy_er}   = parse_number_id(text=>$2);
         $i++;
     }
     $i == $num_items or
-        return [500, "Check: num of rows in DD/TT table != Bank Notes table"];
+        return [543, "Check: #rows in Mata Uang table != Bank Notes table"];
+
+    $i = 0;
+    while ($ttc_table   =~ m{
+                                <td[^>]+>([0-9.,]+)</td>\s+
+                                <td[^>]+>([0-9.,]+)</td>\s*
+                                (?:<!--.+?-->)?\s*</tr>
+                       }xsg) {
+        $items[$i]{sell_ttc} = parse_number_id(text=>$1);
+        $items[$i]{buy_ttc}  = parse_number_id(text=>$2);
+        $i++;
+    }
+    $i == $num_items or
+        return [543, "Check: #rows in Mata Uang table != TT Counter table"];
+
+    $i = 0;
+    while ($bn_table   =~ m{
+                               <td[^>]+>([0-9.,]+)</td>\s+
+                               <td[^>]+>([0-9.,]+)</td>\s*
+                               (?:<!--.+?-->)?\s*</tr>
+                       }xsg) {
+        $items[$i]{sell_bn}  = parse_number_id(text=>$1);
+        $items[$i]{buy_bn}   = parse_number_id(text=>$2);
+        $i++;
+    }
+    $i == $num_items or
+        return [543, "Check: #rows in Mata Uang table != Bank Notes table"];
 
     my %items;
     for (@items) {
@@ -63,23 +99,38 @@ sub get_currencies {
     [200, "OK", {update_date=>undef, currencies=>\%items}];
 }
 
+our $_get_res;
+
 sub convert_currency {
     my ($n, $from, $to) = @_;
 
-    my $res = get_currencies();
-    return undef if $res->[0] != 200;
-    return undef unless uc($to) eq 'IDR';
+    # XXX set expiry
+    if (!$_get_res) {
+        $_get_res = get_currencies();
+        warn "Can't get currencies: $_get_res->[0] - $_get_res->[1]\n";
+        return undef;
+    }
+    if ($_get_res->[0] != 200) {
+        warn "get currencies failed: $_get_res->[0] - $_get_res->[1]\n";
+        return undef;
+    }
+    if (uc($to) ne 'IDR') {
+        die "Currently only conversion to IDR is supported".
+            " (you asked for conversion to '$to')\n";
+    }
 
-    my $c = $res->[2]{currencies}{uc $from} or return undef;
-    $n * ($c->{sell_ddtt} + $c->{buy_ddtt}) / 2;
+    my $c = $_get_res->[2]{currencies}{uc $from} or return undef;
+    $n * ($c->{sell_bn} + $c->{buy_bn}) / 2;
 }
 
 1;
 # ABSTRACT: Convert currencies using KlikBCA
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -87,7 +138,7 @@ Finance::Currency::Convert::KlikBCA - Convert currencies using KlikBCA
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
@@ -95,29 +146,25 @@ version 0.02
 
  printf "1 USD = Rp %.0f\n", convert_currency(1, 'USD', 'IDR');
 
-=head1 TODO
-
-=over 4
-
-=item * Currently can only handle conversion I<to> IDR
-
-=item * Parse last update time
-
-=back
-
-=head1 SEE ALSO
-
-L<http://www.klikbca.com/>
-
 =head1 DESCRIPTION
-
-
-This module has L<Rinci> metadata.
 
 =head1 FUNCTIONS
 
+=head2 convert_currency($amount, $from, $to) => NUM
 
-None are exported by default, but they are exportable.
+Currently can only handle conversion *to* IDR. Dies if given other currency.
+
+Will warn if failed getting currencies from the webpage.
+
+Currency rate is cached.
+
+Currently uses the Bank Notes rate.
+
+Will return undef if no conversion rate is available for the requested currency.
+
+Use get_currencies(), which actually retrieves and scrapes the source web page,
+if you need the more complete result.
+
 
 =head2 get_currencies() -> [status, msg, result, meta]
 
@@ -129,16 +176,43 @@ Return value:
 
 Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
 
+=head1 TODO
+
+=over
+
+=item * Parse last update time
+
+=back
+
+=head1 SEE ALSO
+
+L<http://www.klikbca.com/>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Finance-Currency-Convert-KlikBCA>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Finance-Currency-Convert-KlikBCA>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Finance-Currency-Convert-KlikBCA>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =head1 AUTHOR
 
 Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
